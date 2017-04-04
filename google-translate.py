442b8f362b7eabdb1e3d45a5bfcb2d736dc48bd3
George Hunt <georgejhunt@gmail.com>
2017-04-04 01:31:27 -0700

    remove images

--- google-translate.py
+++ google-translate.py
@@ -4,7 +4,7 @@
 import sys
 
 if len(sys.argv) < 2:
-  print "First parameter is input filename"
+  print "First parameter is translation filename"
   sys.exit(1)
 fd = open(sys.argv[1],"r")
 raw = fd.read()
@@ -25,14 +25,17 @@ while index < len(raw) and index != -1:
                  spanish.append(raw[index:endnibble])
                  index = endnibble + 7
 # we are done with the input file
+print "length of english array is %s"%len(english)
+print "length of raw is %s"%len(raw)
+if len(sys.argv) < 3:
+  print "Second parameter is clean input filename"
+  sys.exit(1)
 raw = ''
 fd = open(sys.argv[2],"r")
 raw = fd.read()
 outstr = ''
 index = 0
 cursor = 0
-print "length of english array is %s"%len(english)
-print "length of raw is %s"%len(raw)
 for i in range(len(english)):
    if english[i].find("href=") != -1:
        continue
